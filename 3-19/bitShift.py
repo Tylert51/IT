b504c9b5b1a1d364f76fd3102b4249f4ada0c62a
tyler <ttang7910@bths.edu>
2024-03-19 21:41:23 -0400

    finished bitshift algorithm

--- 3-19/bitShift.py
+++ 3-19/bitShift.py
@@ -1,3 +1,19 @@
-def shiftLeft(str, n):
+bits = input("Enter a bit: ")
+num = int(input("How many places do you want it to be shifted: "))
+
+def shiftLeft(bits, n):
     for i in range(0, n):
-        bits = 
+        bits = bits[1:] + bits[0:1]
+
+    print("left shift:", bits)
+
+def shiftRight(bits, n):
+    for i in range(0, n):
+        bits = bits[len(bits) - 1 : len(bits)] + bits[0 : len(bits) - 1]
+
+    print("right shift:", bits)
+
+
+shiftLeft(bits, num)
+shiftRight(bits, num)
+
