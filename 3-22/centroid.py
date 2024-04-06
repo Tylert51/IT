2940d5c27195f5c11ade8d0fdee9fbb077ccfa69
tyler <ttang7910@bths.edu>
2024-04-06 00:40:00 -0400

    finished centroid.py

--- 3-22/centroid.py
+++ 3-22/centroid.py
@@ -3,7 +3,6 @@ import pandas as pd
 import random
 coords = pd.read_csv("3-22/coordinates.csv")
 
-
 xCoords = coords['xCoords']
 yCoords = coords['yCoords']
 
@@ -24,7 +23,6 @@ def getClusterPoints(n):
 
     return clusterPoints
 
-
 def calcSquaredDistance(x1, y1, x2, y2):
     xDiff = x2 - x1
     yDiff = y2 - y1
@@ -33,53 +31,77 @@ def calcSquaredDistance(x1, y1, x2, y2):
 
     return dist
 
-clusterPoints = getClusterPoints(2)
+numClusters = int(input("\n\nHow many clusters do you want: "))
+clusterPoints = getClusterPoints(numClusters)
 
+centroids = []
 
-for i in range (len(xCoords)):
-    minIndex = 0
-    
-    xCoord = xCoords[i]
-    yCoord = yCoords[i]
+def getAllClusterPoints():
 
-    minDist = calcSquaredDistance(xCoord, yCoord, clusterPoints[0][0], clusterPoints[0][1])
-
-    for c in range (1, len(clusterPoints) ):
-        dist = calcSquaredDistance(xCoord, yCoord, clusterPoints[c][0], clusterPoints[c][1])
-        if(dist < minDist):
-            minDist = dist
-            minIndex = c
-
-
-    allClusterPoints[minIndex].append([xCoord, yCoord])
-
-print(allClusterPoints)
-
-
-    
-
+    for i in range (len(xCoords)):
+        minIndex = 0
         
+        xCoord = xCoords[i]
+        yCoord = yCoords[i]
 
+        minDist = calcSquaredDistance(xCoord, yCoord, clusterPoints[0][0], clusterPoints[0][1])
 
+        for c in range (1, len(clusterPoints) ):
+            dist = calcSquaredDistance(xCoord, yCoord, clusterPoints[c][0], clusterPoints[c][1])
+            if(dist < minDist):
+                minDist = dist
+                minIndex = c
 
+        allClusterPoints[minIndex].append([xCoord, yCoord])
 
-# for i in range(0, len(xCoords)):
+    for cluster in allClusterPoints:
+        xSum = 0
+        ySum = 0
+        numPts = len(cluster)
+
+        for point in cluster:
+            xSum += point[0]
+            ySum += point[1]
+
+        centroids.append([xSum/numPts, ySum/numPts])
+
+getAllClusterPoints()
+
+reassignment = True
+numLoop = 0
+
+while reassignment == True:
+
+    reassignment = False
+    totalDist = 0
+
+    for i in range (len(allClusterPoints)):
+
+        dist = calcSquaredDistance(allClusterPoints[i][0][0], allClusterPoints[i][0][1], centroids[i][0], centroids[i][1])
+        totalDist += dist
+
+        for j in range (1, len(allClusterPoints[i])):
+
+            totalDist += calcSquaredDistance(allClusterPoints[i][j][0], allClusterPoints[i][j][1], centroids[i][0], centroids[i][1])
+
+            for k in range (len(centroids)):
+                if (k != i):
+                    newDist = calcSquaredDistance(allClusterPoints[i][j][0], allClusterPoints[i][j][1], centroids[k][0], centroids[k][1])
+                    if (newDist < dist): 
+                        reassignment = True
+                        break
+                    
+            if(reassignment):
+                break
+        if(reassignment):
+            break
     
-#     distA = calcSquaredDistance(ax, ay, xCoords[i], yCoords[i])
-#     distB = calcSquaredDistance(bx, by, xCoords[i], yCoords[i])
+    numLoop += 1
+    allClusterPoints = []
+    centroids = []
+    clusterPoints = getClusterPoints(numClusters)
+    getAllClusterPoints()
 
-#     if distA < distB: 
-#         aCluster.append(i)
-#         aDist += distA
-#     else:
-#         bCluster.append(i)
-#         bDist += distB
-
-
-# print("\nLatitude 1: ", ax)
-# print("Longitude 1: ", ay, "\n")
-
-# print("Latitude 2: ", bx)
-# print("Longitude 2: ", by, "\n")
-
-# print("Squared Distance: ", aDist + bDist, "\n")
+print("\nNum Iterrations: ", numLoop)
+print("\nCentroids: ", centroids)
+print("\nSum of squared Distances: ", totalDist, "\n\n")
